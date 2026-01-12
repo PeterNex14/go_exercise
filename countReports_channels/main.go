575cd6a03d8616189c66177e6448b1cf9ca63d2f
PeterNex14 <peternex42@gmail.com>
2026-01-12 13:17:10 +0800

    do countReports exercise channels

---
+++ countReports_channels/main.go
@@ -0,0 +1,24 @@
+package main
+
+func countReports(numSentCh chan int) int {
+	count := 0
+	for {
+		value, ok := <- numSentCh
+		if !ok {
+			break
+		} else {
+			count += value
+		}
+	}
+	return count
+}
+
+// don't touch below this line
+
+func sendReports(numBatches int, ch chan int) {
+	for i := 0; i < numBatches; i++ {
+		numReports := i*23 + 32%17
+		ch <- numReports
+	}
+	close(ch)
+}
